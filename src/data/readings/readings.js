6db78b6800ed4bf4522bb23d57cc8fc43b3cdb61
Ronan Gaillard <ronan.gaillard@live.fr>
2017-02-14 21:12:00 +0100

    Add The Linux Kernel Dev book

--- src/data/readings/readings.js
+++ src/data/readings/readings.js
@@ -10,8 +10,14 @@ var readings = {
     {
         title: 'Zero To One',
         author: 'Peter Thiel',
-        preview: ' An interesting read with insights for tech startups.',
+        preview: 'An interesting read with insights for tech startups. Thiel shares a bounty of insights from his career as a founder of Paypal. All in all this book is a collection of Silicon Valley business wisdom for the startup world, based on a course that he taught at Stanford in 2012.',
         image: 'https://03fcd67fd51850d3ba6b-6cb392df11a341bce8c76b1898d0c030.ssl.cf3.rackcdn.com/large/9780/7535/9780753555200.jpg'
+    },
+    {
+        title: 'The Linux Kernel Development',
+        author: 'Robert Love',
+        preview: 'Master Linux from the deepest components. A must-read especially if you want to build and understand modern embedded systems as well as most Linux vulnerabilities.',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/51zqofYYmsL._SX258_BO1,204,203,200_.jpg'
     }]
 }
 
